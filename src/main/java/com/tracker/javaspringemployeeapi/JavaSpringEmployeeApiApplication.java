d655fc59d4078df481d8ff05040c3796fdad05de
TOVTC <veronica.tc.to@outlook.com>
2022-10-13 03:00:20 -0400

    initial commit

---
+++ src/main/java/com/tracker/javaspringemployeeapi/JavaSpringEmployeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.javaspringemployeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringEmployeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringEmployeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tracker/javaspringemployeeapi/JavaSpringEmployeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tracker.javaspringemployeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringEmployeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
